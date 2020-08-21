40824ae62a849fe0601fd0175579459e234560ea
zhangqiulu <68528259+ZhangQiuLu-SmartNews@users.noreply.github.com>
2020-08-21 16:54:36 +0800

    add train shell

--- recall/i2i/w2v/train_w2v.sh
+++ recall/i2i/w2v/train_w2v.sh
@@ -1,3 +1,3 @@
 input=$1
 output=$2
-nohup python3 -m train.w2v_train --input w2v_train_data/$input --output w2v_train_result/Sinput.$output > w2v_train_log/Sinput.$output.log
+nohup python3 -m train.w2v_train --input w2v_train_data/$input --output w2v_train_result/Sinput.$output > w2v_train_log/Sinput.$output.log &
