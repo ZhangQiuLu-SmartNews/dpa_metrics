332bc79962c0a4c720c1e8198c4d76bbb2f0893d
zhangqiulu <68528259+ZhangQiuLu-SmartNews@users.noreply.github.com>
2020-08-21 15:09:43 +0800

    download w2v data

--- recall/i2i/w2v/data/download_w2v_data.sh
+++ recall/i2i/w2v/data/download_w2v_data.sh
@@ -1,6 +1,7 @@
 set +e
 set -x
 dt=$1
+path=`pwd`
 cd ~/smart-ad-dmp/azkaban-flow/datafeed
 s3_prefix="smartad-dmp/warehouse/user/seanchuang/i2i_offline_w2v_train_data/dt=${dt}"
 w2v_data="/mnt1/train/${s3_prefix}/merged.data"
@@ -10,5 +11,5 @@ fi
 bash bin/s3_sync_mnt "/mnt1/train" ${s3_prefix}
 du -khs ${w2v_data}
 
-mv $w2v_data `pwd`
+mv $w2v_data $path
 
