904d863bd48c6505be15edfbf495fbfdb20598f1
zhangqiulu <68528259+ZhangQiuLu-SmartNews@users.noreply.github.com>
2020-08-21 16:30:40 +0800

    add similarity

--- recall/i2i/w2v/train/w2v_train.py
+++ recall/i2i/w2v/train/w2v_train.py
@@ -1,15 +1,27 @@
 from fasttext import train_unsupervised
 import tqdm
 import argparse
+import pandas as pd
+import numpy as np
 
 
 def get_similarity(model, args):
     similarity = {}
-    with tqdm.tqdm(total=len(model.words)) as progress:
+    with tqdm.tqdm(desc="get_similarity", total=len(model.words)) as progress:
         for word in model.words:
-            similarity[word] = ['{}={}'.format(cscore, cword) for cscore, cword in model.get_nearest_neighbors(word, k=args.k)]
+            similarity[word] = ['{}={}'.format(cword, cscore) for cscore, cword in model.get_nearest_neighbors(word, k=args.k)]
             progress.update(1)
     return similarity
+
+
+def save_csv(similarity, args):
+    topk_df = pd.DataFrame({'item': np.sort(similarity.keys())})
+    with tqdm.tqdm(desc='save_csv', total=len(similarity.keys()) // args.save_one_time) as progress:
+        for i in range(len(similarity.keys()), step=args.save_one_time):
+            topk_df['topk'] = topk_df.iloc[i: i+args.save_one_time]['item'].str.apply(lambda x: similarity[x])
+            topk_df.to_csv(args.outputfile, mode='a', header=False, index=False)
+            progress.update(1)
+
 
 def main(args):
     model = train_unsupervised(
@@ -26,6 +38,7 @@ if __name__ == "__main__":
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('--input', type=str, default='', dest='inputfile')
+    parser.add_argument('--output', type=str, default='', dest='outputfile')
     parser.add_argument('--topk', type=int, default=10, dest='k')
     args = parser.parse_args()
 
