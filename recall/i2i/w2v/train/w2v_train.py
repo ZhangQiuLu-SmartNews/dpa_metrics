28d5cd908b83cf8a130c428f88e483f4aec8652e
zhangqiulu <68528259+ZhangQiuLu-SmartNews@users.noreply.github.com>
2020-08-21 16:11:41 +0800

    add similarity

--- recall/i2i/w2v/train/w2v_train.py
+++ recall/i2i/w2v/train/w2v_train.py
@@ -1,21 +1,31 @@
 from fasttext import train_unsupervised
-import os
+import tqdm
 import argparse
 
+
+def get_similarity(model, args):
+    similarity = {}
+    with tqdm.tqdm(total=len(model.words)) as progress:
+        for word in model.words:
+            similarity[word] = ['{}={}'.format(cscore, cword) for cscore, cword in model.get_nearest_neighbors(word, k=args.k)]
+            progress.update(1)
 
 def main(args):
     model = train_unsupervised(
         input=args.inputfile,
         model='skipgram',
     )
-    print(model.words)
+
+    similarity_dict = get_similarity(model, args)
+    print(similarity_dict)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="fuck i2i",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--input', type=str, default='test.csv', dest='inputfile')
+    parser.add_argument('--input', type=str, default='', dest='inputfile')
+    parser.add_argument('--topk', type=int, default=10, dest='k')
     args = parser.parse_args()
 
     main(args)
